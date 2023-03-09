e067cffde90de356d7a984d34e372b44e020d90b
jDudek <jakub.dudek94@gmail.com>
2023-03-09 23:36:44 +0100

    ViewSingleRoom.js component adjusted for new features (calendar + googleMap)

--- app/components/ViewSingleRoom.js
+++ app/components/ViewSingleRoom.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState, useContext } from "react"
-import { Link, useParams, useNavigate, useLocation } from "react-router-dom"
+import { Link, useLocation } from "react-router-dom"
 import ReactTooltip from "react-tooltip"
 import Axios from "axios"
 import { useImmer } from "use-immer"
@@ -8,22 +8,16 @@ import StateContext from "../StateContext"
 import DispatchContext from "../DispatchContext"
 import Loading from "./Loading"
 import NotFound from "./NotFound"
-import moment from "moment/moment"
-import LikeButton from "./LikeButton"
 import RenderAvatar from "./Avatar"
 import DeleteModal from "./DeleteModal"
-import RefreshButton from "./RefreshButton"
 import EditRoomBasic from "./EditRoomBasic"
-import Calendar from "./Calendar"
-import SubmitOrCancelButton from "./SubmitOrCancelButton"
+import CustomMaterialSymbol from "./CustomMaterialSymbol"
+import EditRoomDates from "./EditRoomDates"
+import Map from "./Map"
 
 function ViewSingleRoom(props) {
   const location = useLocation()
   const [navigated, setNavigated] = useState(false)
-  const { id } = useParams()
-  const [admins, setAdmins] = useState([])
-  const [isUserAdmin, setIsUserAdmin] = useState(false)
-  const [refreshRequest, setRefreshRequest] = useState(false)
   const appDispatch = useContext(DispatchContext)
   const appState = useContext(StateContext)
   const [room, setRoom] = useImmer({
@@ -31,20 +25,40 @@ function ViewSingleRoom(props) {
     name: "",
     description: "",
     isPublic: false,
+    isUserAdmin: false,
+    isUserMember: false,
+    admins: [],
+    members: [],
+    location: {
+      city: "",
+      zipCode: "",
+      street: "",
+      number: "",
+      latitude: "",
+      longitude: ""
+    }
+  })
+  const [roomDates, setRoomDates] = useImmer({
     nextMatchDate: "",
     nextMatchTime: {
       hour: "",
       minute: ""
     },
-    location: {
-      city: "",
-      zipCode: "",
-      street: "",
-      number: ""
+    nextMatchRegistrationOpenDate: "",
+    nextMatchRegistrationOpenTime: {
+      hour: "",
+      minute: ""
+    },
+    nextMatchRegistrationEndDate: "",
+    nextMatchRegistrationEndTime: {
+      hour: "",
+      minute: ""
     }
   })
   const [state, setState] = useImmer({
-    renderCalendar: false,
+    renderEditDates: false,
+    renderMap: false,
+    renderDescription: true,
     renderEditSubPage: false,
     renderBalancerSubPage: false,
     renderPlayerSubPage: false,
@@ -56,6 +70,7 @@ function ViewSingleRoom(props) {
       if (location.state === undefined || location.state == null) return
       if (location.state.navigated) {
         const responseData = location.state.responseData
+        const membersArray = responseData.users
         const adminsArray = responseData.admins
 
         setNavigated(true)
@@ -64,35 +79,101 @@ function ViewSingleRoom(props) {
           draft.name = responseData.name
           draft.description = responseData.description == null ? "" : responseData.description
           draft.isPublic = responseData.public
+          draft.isUserAdmin = adminsArray.filter(admin => admin.id == appState.user.id).length > 0
+          draft.isUserMember = membersArray.filter(member => member.id == appState.user.id).length > 0
+          draft.admins = adminsArray
+          draft.members = membersArray
           draft.location.city = responseData.location.city
           draft.location.zipCode = responseData.location.zipCode
           draft.location.street = responseData.location.street
           draft.location.number = responseData.location.number
+          draft.location.latitude = responseData.location.latitude
+          draft.location.longitude = responseData.location.longitude
+        })
 
+        setRoomDates(draft => {
           if (responseData.nextMatchDate) {
             draft.nextMatchDate = responseData.nextMatchDate.split("T")[0]
             draft.nextMatchTime.hour = responseData.nextMatchDate.split("T")[1].split(":")[0]
             draft.nextMatchTime.minute = responseData.nextMatchDate.split("T")[1].split(":")[1]
           }
+          if (responseData.nextMatchRegistrationStartDate) {
+            draft.nextMatchRegistrationOpenDate = responseData.nextMatchRegistrationStartDate.split("T")[0]
+            draft.nextMatchRegistrationOpenTime.hour = responseData.nextMatchRegistrationStartDate.split("T")[1].split(":")[0]
+            draft.nextMatchRegistrationOpenTime.minute = responseData.nextMatchRegistrationStartDate.split("T")[1].split(":")[1]
+          }
+          if (responseData.nextMatchRegistrationEndDate) {
+            draft.nextMatchRegistrationEndDate = responseData.nextMatchRegistrationEndDate.split("T")[0]
+            draft.nextMatchRegistrationEndTime.hour = responseData.nextMatchRegistrationEndDate.split("T")[1].split(":")[0]
+            draft.nextMatchRegistrationEndTime.minute = responseData.nextMatchRegistrationEndDate.split("T")[1].split(":")[1]
+          }
         })
-        setAdmins(admins)
-        setIsUserAdmin(adminsArray.filter(admin => admin.id == appState.user.id).length > 0)
       }
     }
     fetchData()
   }, [])
 
-  function showUploadPhotoButton() {
-    if (isUserAdmin) {
+  function getStyleForBasicInfoSpan(color) {
+    return { color: color, whiteSpace: "nowrap", fontVariant: "all-small-caps" }
+  }
+
+  function renderUploadPhotoButton() {
+    if (room.isUserAdmin) {
       return (
         <div className="d-flex flex-row mt-3 ml-3">
           <Link to={""} data-tip="Upload" data-for="upload" className="text-primary mr-2">
-            <span className="material-symbols-outlined link-black ml-auto"> upload </span>
+            <span className="material-symbols-outlined link-black ml-auto" style={{ color: "#373991" }}>
+              {" "}
+              upload{" "}
+            </span>
           </Link>
           <ReactTooltip id="upload" className="custom-tooltip" />
         </div>
       )
     }
+  }
+
+  useEffect(() => {
+    if (!state.renderMap && !state.renderEditDates) {
+      setTimeout(() => {
+        setState(draft => {
+          draft.renderDescription = true
+        })
+      }, 1300)
+    } else {
+      setState(draft => {
+        draft.renderDescription = false
+      })
+    }
+  }, [state.renderMap, state.renderEditDates])
+
+  useEffect(() => {
+    setState(draft => {
+      draft.renderMap = false
+      draft.renderEditDates = false
+    })
+  }, [state.renderEditSubPage, state.renderBalancerSubPage, state.renderPlayerSubPage, state.renderSettingsSubPage])
+
+  function renderMapSwitcher() {
+    setState(draft => {
+      ;(draft.renderEditDates = false), (draft.renderDescription = false)
+    })
+    setTimeout(() => {
+      setState(draft => {
+        draft.renderMap = true
+      })
+    }, 1000)
+  }
+
+  function renderEditDatesSwitcher() {
+    setState(draft => {
+      ;(draft.renderMap = false), (draft.renderDescription = false)
+    })
+    setTimeout(() => {
+      setState(draft => {
+        draft.renderEditDates = true
+      })
+    }, 1000)
   }
 
   function resetRenderedSubPages() {
@@ -102,53 +183,6 @@ function ViewSingleRoom(props) {
       draft.renderPlayerSubPage = false
       draft.renderSettingsSubPage = false
     })
-  }
-
-  function handleNextMatchDateEdit(data) {
-    if (isUserAdmin) {
-      setRoom(draft => {
-        draft.nextMatchDate = data
-      })
-    }
-  }
-
-  function handleNextMatchHourEdit(e) {
-    if (isUserAdmin) {
-      const newHours = parseInt(e.target.value).toString().padStart(0, "0")
-      setRoom(draft => {
-        draft.nextMatchTime.hour = newHours
-      })
-    }
-  }
-
-  function handleNextMatchMinuteEdit(e) {
-    if (isUserAdmin) {
-      const newMinutes = parseInt(e.target.value).toString().padStart(0, "0")
-      setRoom(draft => {
-        draft.nextMatchTime.minute = newMinutes
-      })
-    }
-  }
-
-  async function handleSubmitNewMatchDate() {
-    if (isUserAdmin) {
-      const nextMatchDateTimeString = `${room.nextMatchDate}T${room.nextMatchTime.hour}:${room.nextMatchTime.minute}:00.000Z`
-
-      try {
-        await Axios.patch(`/api/room/next-match-date/${room.id}?adminId=${appState.user.id}&date=${nextMatchDateTimeString}`, {}, { headers: { Authorization: `Bearer ${appState.user.token}` } })
-      } catch (e) {
-        console.log("There was a problem or the request was cancelled." + e)
-        return
-      }
-      appDispatch({
-        type: "flashMessage",
-        value: "New date for next match set successfully!",
-        messageType: "message-green"
-      })
-      setState(draft => {
-        draft.renderCalendar = false
-      })
-    }
   }
 
   function handleRoomEdit(data) {
@@ -163,33 +197,21 @@ function ViewSingleRoom(props) {
     })
   }
 
-  function renderTimeSelector() {
-    return (
-      <div className="mt-2">
-        <select className="ml-2" value={room.nextMatchTime.hour} onChange={handleNextMatchHourEdit}>
-          {Array.from(Array(24).keys()).map(value => (
-            <option key={value} value={value}>
-              {value.toString().padStart(2, "0")}
-            </option>
-          ))}
-        </select>{" "}
-        <select className="ml-2" value={room.nextMatchTime.minute} onChange={handleNextMatchMinuteEdit}>
-          {Array.from(Array(60).keys()).map(value => (
-            <option key={value} value={value}>
-              {value.toString().padStart(2, "0")}
-            </option>
-          ))}
-        </select>
-        <SubmitOrCancelButton
-          handleSubmitNewMatchDate={handleSubmitNewMatchDate}
-          handleCancelNewMatchDate={() =>
-            setState(draft => {
-              draft.renderCalendar = false
-            })
-          }
-        />
-      </div>
-    )
+  function handleDatesUpdate(data) {
+    setRoomDates(draft => {
+      draft.nextMatchDate = data.nextMatchDate
+      draft.nextMatchTime.hour = data.nextMatchTime.hour
+      draft.nextMatchTime.minute = data.nextMatchTime.minute
+      draft.nextMatchRegistrationOpenDate = data.nextMatchRegistrationOpenDate
+      draft.nextMatchRegistrationOpenTime.hour = data.nextMatchRegistrationOpenTime.hour
+      draft.nextMatchRegistrationOpenTime.minute = data.nextMatchRegistrationOpenTime.minute
+      draft.nextMatchRegistrationEndDate = data.nextMatchRegistrationEndDate
+      draft.nextMatchRegistrationEndTime.hour = data.nextMatchRegistrationEndTime.hour
+      draft.nextMatchRegistrationEndTime.minute = data.nextMatchRegistrationEndTime.minute
+    })
+    setState(draft => {
+      draft.renderEditDates = false
+    })
   }
 
   if (!navigated) return <NotFound />
@@ -202,10 +224,15 @@ function ViewSingleRoom(props) {
         <Link className="text-primary medium font-weight-bold" to={`/`}>
           &laquo; Back HOME
         </Link>
-
         <div className="mobile-toggle">
           <h2 className="d-flex ml-auto mobile-toggle">
-            <div className="d-flex ml-3 p-2 align-items-center" data-tip={room.isPublic ? "ROOM IS PUBLIC" : "ROOM IS PRIVATE"} data-for={"acccess"}>
+            <div className="d-flex  p-2 align-items-center" data-tip={room.isUserMember ? "YOU'RE MEMBER" : "YOU AREN'T MEMBER YET. SEND REQUEST NOW!"} data-for={"acccess"}>
+              <span className="material-symbols-outlined " style={{ fontSize: "25px", color: room.isUserMember ? "darkgreen" : "darkred", cursor: "pointer" }}>
+                {room.isUserMember ? "person" : "person_add"}
+              </span>
+              <ReactTooltip id={"acccess"} className="custom-tooltip" style={{ fontVariant: "small-caps", position: "static" }} delayShow={500} />
+            </div>
+            <div className="d-flex p-2 align-items-center" data-tip={room.isPublic ? "ROOM IS PUBLIC" : "ROOM IS PRIVATE"} data-for={"acccess"}>
               <span className="material-symbols-outlined mr-2" style={{ fontSize: "25px", color: room.isPublic ? "darkgreen" : "darkred", cursor: "pointer" }}>
                 {room.isPublic ? "lock_open" : "lock"}
               </span>
@@ -224,43 +251,68 @@ function ViewSingleRoom(props) {
           </h2>
         </div>
         <div className="content d-flex flex-column mt-1">
-          <div className="content single-room-layout">
+          <div className="content single-room-layout d-flex mb-2">
             <div className="single-room-layout-row">
-              <div className="profile-avatar d-flex flex-column align-items-center">
+              <div className="profile-avatar d-flex mr-4 flex-column align-items-center">
                 <RenderAvatar />{" "}
               </div>
               <div className="d-flex flex-row" style={{ marginLeft: "3rem" }}>
-                {showUploadPhotoButton()}
+                {renderUploadPhotoButton()}
               </div>
             </div>
-            <div className="room-content container mr-3">{room.description}</div>
-            <CSSTransition in={state.renderCalendar} timeout={700} classNames="add-room-text" unmountOnExit>
-              <div className="d-flex flex-column mt-3 justify-content-center">
-                <Calendar chosenDate={handleNextMatchDateEdit} />
-                {isUserAdmin && renderTimeSelector()}
-              </div>
-            </CSSTransition>
-            <div className="d-flex flex-column mr-2 ml-4 align-items-center">
-              <span style={{ color: "white", whiteSpace: "nowrap", fontVariant: "all-small-caps" }}>Next match on:</span>
-              <span style={{ color: "wheat", whiteSpace: "nowrap", fontVariant: "all-small-caps" }}>{room.nextMatchDate ? room.nextMatchDate : "N/A"}</span>
-              <span style={{ color: "wheat", whiteSpace: "nowrap", fontVariant: "all-small-caps" }}>{room.nextMatchTime.hour ? room.nextMatchTime.hour + " : " + room.nextMatchTime.minute : "N/A"}</span>
-              <span
-                style={{ color: "#2f3061" }}
-                className="material-symbols-outlined mt-3"
-                data-tip={"CHECK NEXT MATCH DATE"}
-                data-for={"calendar"}
-                onClick={() =>
-                  setState(draft => {
-                    draft.renderCalendar = !draft.renderCalendar
-                  })
-                }
-              >
+            <div className="room-content container">
+              {state.renderEditDates == false && state.renderMap == false && (
+                <CSSTransition in={state.renderDescription} timeout={1300} classNames="room-description" unmountOnExit>
+                  <div>{room.description}</div>
+                </CSSTransition>
+              )}
+              <CSSTransition in={state.renderEditDates} timeout={1000} classNames="add-calendar" unmountOnExit>
+                <div>
+                  <EditRoomDates
+                    roomId={room.id}
+                    dates={roomDates}
+                    onSubmit={handleDatesUpdate}
+                    onCancel={() =>
+                      setState(draft => {
+                        draft.renderEditDates = false
+                      })
+                    }
+                    renderCondition={state.renderEditDates}
+                  />
+                </div>
+              </CSSTransition>
+              <CSSTransition in={state.renderMap} timeout={1000} classNames="add-map" unmountOnExit>
+                <>
+                  <Map location={{ lat: room.location.latitude, lng: room.location.longitude }} />
+                </>
+              </CSSTransition>
+            </div>
+
+            <div className="d-flex flex-column mr-2 ml-4 mt-1 align-items-center">
+              <span style={getStyleForBasicInfoSpan("white")}>Next match on:</span>
+              <span style={getStyleForBasicInfoSpan("wheat")}>{roomDates.nextMatchDate ? roomDates.nextMatchDate : "N/A"}</span>
+              <span style={getStyleForBasicInfoSpan("wheat")}>{roomDates.nextMatchTime.hour ? roomDates.nextMatchTime.hour + " : " + roomDates.nextMatchTime.minute : "N/A"}</span>
+              <span style={{ color: "#373991" }} className="material-symbols-outlined mt-2 mb-4" data-tip={"CHECK NEXT MATCH DATE"} data-for={"calendar"} onClick={renderEditDatesSwitcher}>
                 calendar_month
               </span>
-              <ReactTooltip id={"calendar"} className="custom-tooltip" delayShow={500} />
+              <ReactTooltip id={"calendar"} className="custom-tooltip" delayShow={1000} />
+
+              <span style={getStyleForBasicInfoSpan("white")}>Registration opens on:</span>
+              <span style={getStyleForBasicInfoSpan("wheat")}>{roomDates.nextMatchRegistrationOpenDate ? roomDates.nextMatchRegistrationOpenDate : "N/A"}</span>
+              <span style={getStyleForBasicInfoSpan("wheat")}>{roomDates.nextMatchRegistrationOpenTime.hour ? roomDates.nextMatchRegistrationOpenTime.hour + " : " + roomDates.nextMatchRegistrationOpenTime.minute : "N/A"}</span>
+              <span style={{ marginTop: "20px" }}></span>
+              <span style={getStyleForBasicInfoSpan("white")}>Registration ends on:</span>
+              <span style={getStyleForBasicInfoSpan("wheat")}>{roomDates.nextMatchRegistrationEndDate ? roomDates.nextMatchRegistrationEndDate : "N/A"}</span>
+              <span style={getStyleForBasicInfoSpan("wheat")}>{roomDates.nextMatchRegistrationEndTime.hour ? roomDates.nextMatchRegistrationEndTime.hour + " : " + roomDates.nextMatchRegistrationEndTime.minute : "N/A"}</span>
             </div>
           </div>
+          <div className="d-flex flex-row mt-1 justify-content-center align-items-center">
+            <CustomMaterialSymbol name={"map"} fontSize={"30px"} fontColor={"#373991"} cursor={"pointer"} onClick={renderMapSwitcher} />
+            <span style={getStyleForBasicInfoSpan("wheat")}>{room.location.city ? room.location.city : "N/A"}</span>
+            <span style={getStyleForBasicInfoSpan("wheat")}>{room.location.street ? ", st. " + room.location.street + " " + room.location.number : ", N/A"}</span>
+          </div>
         </div>
+
         <ul className="menu" style={{ fontVariant: "all-small-caps", cursor: "pointer" }}>
           <li>
             <a
@@ -299,7 +351,7 @@ function ViewSingleRoom(props) {
               Settings
             </a>
           </li>
-          {isUserAdmin && (
+          {room.isUserAdmin && (
             <li>
               <a
                 onClick={() => {
