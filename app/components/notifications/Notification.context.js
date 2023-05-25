7703661e3416811d74664548c777501d3f949475
jDudek <jakub.dudek94@gmail.com>
2023-05-25 15:35:30 +0200

    NotificationContext update - adjusted to back-end notification system

--- app/components/notifications/Notification.context.js
+++ app/components/notifications/Notification.context.js
@@ -1,25 +1,21 @@
-import React, { useContext, useEffect, useState } from "react";
-import Axios from "axios";
-import StateContext from "../../StateContext";
+import React, { useContext, useEffect, useState } from "react"
+import Axios from "axios"
+import StateContext from "../../StateContext"
 
-const NotificationContext = React.createContext();
+const NotificationContext = React.createContext()
 
 export const NotificationProvider = ({ children }) => {
-  const [notifications, setNotifications] = useState([]);
-  const appState = useContext(StateContext);
-  const currentUserId = appState.user.id;
+  const [notifications, setNotifications] = useState([])
+  const appState = useContext(StateContext)
+  const currentUserId = appState.user.id
 
-  // useEffect(() => {
-  //   Axios.get(
-  //     `https://localhost:444/notification-service/all?userId=${currentUserId}`
-  //   ).then((response) => setNotifications(response.data));
-  // }, []);
+  useEffect(() => {
+    if (appState.isLoggedIn) {
+      Axios.get(`/api/notification/from-db/private-user?userId=${currentUserId}`, { headers: { Authorization: `Bearer ${appState.user.token}` } }).then(response => setNotifications(response.data))
+    }
+  }, [appState.isLoggedIn])
 
-  return (
-    <NotificationContext.Provider value={{ notifications, setNotifications }}>
-      {children}
-    </NotificationContext.Provider>
-  );
-};
+  return <NotificationContext.Provider value={{ notifications, setNotifications }}>{children}</NotificationContext.Provider>
+}
 
-export default NotificationContext;
+export default NotificationContext
