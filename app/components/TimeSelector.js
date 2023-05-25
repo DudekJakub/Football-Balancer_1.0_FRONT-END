f967e61fd56efdc46cafa067dbd064faca89d545
jDudek <jakub.dudek94@gmail.com>
2023-05-25 15:29:57 +0200

    TimeSelector component - update

--- app/components/TimeSelector.js
+++ app/components/TimeSelector.js
@@ -3,14 +3,14 @@ import React, { useEffect } from "react"
 function TimeSelector(props) {
   return (
     <div className="d-flex flex-row justify-content-center mt-2">
-      <select className="ml-2" value={props.hour} onChange={props.onChangeHour}>
+      <select className="ml-2" defaultValue={props.hour} onChange={props.onChangeHour}>
         {Array.from(Array(24).keys()).map(value => (
           <option key={value} value={value}>
             {value.toString().padStart(2, "0")}
           </option>
         ))}
       </select>{" "}
-      <select className="ml-2" value={props.minute} onChange={props.onChangeMinute}>
+      <select className="ml-2" defaultValue={props.minute} onChange={props.onChangeMinute}>
         {Array.from(Array(60).keys()).map(value => (
           <option key={value} value={value}>
             {value.toString().padStart(2, "0")}
