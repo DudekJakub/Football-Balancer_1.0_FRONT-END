4738627cc4cf461d9790957a4378a86e4e7ab576
jDudek <jakub.dudek94@gmail.com>
2023-05-25 15:43:19 +0200

    RoomManagement component bar added for ViewSingleRoom component

---
+++ app/components/roomManagement/RoomManagement.js
@@ -0,0 +1,22 @@
+import React, { useContext, useEffect, useState } from "react"
+import FormationSection from "./FormationSection"
+import PlayerSection from "./PlayerSection"
+import SkillSection from "./SkillSection"
+
+function RoomManagement(props) {
+  const room = props.room
+
+  return (
+    <form className="d-flex flex-column">
+      <div className="main d-flex flex-column container">
+        <div className="room-management-layout">
+          <PlayerSection room={room} setRoom={props.setRoom} />
+          <SkillSection />
+          <FormationSection />
+        </div>
+      </div>
+    </form>
+  )
+}
+
+export default RoomManagement
