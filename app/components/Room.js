c640c95448b74af98ab9af26affdd9eb0dde3a58
jDudek <jakub.dudek94@gmail.com>
2023-05-25 20:53:07 +0200

    Room component adjusted to RoomContext - fetching data for room moved away to RoomContext

--- app/components/Room.js
+++ app/components/Room.js
@@ -1,6 +1,5 @@
-import React, { useContext } from "react"
+import React, { useContext, useState, useEffect } from "react"
 import { CSSTransition } from "react-transition-group"
-import Axios from "axios"
 import { useImmer } from "use-immer"
 import { Link, useNavigate } from "react-router-dom"
 import StateContext from "../StateContext"
@@ -11,8 +10,8 @@ import { RoomContext } from "./RoomContext"
 function Room(props) {
   const appState = useContext(StateContext)
   const appDispatch = useContext(DispatchContext)
-  const { room2, setRoom } = useContext(RoomContext)
   const navigate = useNavigate()
+  const { room, setRoomId, setRoomPassword, wrongPassword, readyToEnter } = useContext(RoomContext)
   const [state, setState] = useImmer({
     roomId: props.room.id,
     usersSize: props.room.usersInRoomQuantity,
@@ -20,8 +19,9 @@ function Room(props) {
     isUserInRoom: props.room.userInRoom,
     isPublic: props.room.public,
     showPasswordInput: false,
-    providedRoomPassword: "",
-    wrongPasswordMessage: ""
+    renderWrongPasswordMessage: false,
+    renderPasswordInput: false,
+    providedRoomPassword: ""
   })
 
   function getColorForIsUserInRoomIcon() {
@@ -48,38 +48,41 @@ function Room(props) {
     return state.isPublic ? "ROOM IS PUBLIC" : "ROOM IS PRIVATE"
   }
 
-  async function enterTheRoom() {
-    try {
-      const response = await Axios.post(
-        `/api/room/basic-management/enter`,
-        {
-          password: state.providedRoomPassword,
-          roomId: state.roomId,
-          userId: appState.user.id
-        },
-        { headers: { Authorization: `Bearer ${appState.user.token}` } }
-      )
+  function initializeJoiningTheRoom() {
+    setRoomId(state.roomId)
+    setRoomPassword(state.providedRoomPassword)
 
-      if (response.data) {
-        appDispatch({
-          type: "flashMessage",
-          value: "Joined the room!",
-          messageType: "message-green"
-        })
-
-        navigate(`/room/${state.roomId}`, { state: { responseData: response.data, navigated: true } })
-
-        // setRoom(response.data)
-      }
-    } catch (e) {
-      console.log("There was a problem or the request was cancelled." + e)
-
-      if (e.response.status === 401) {
-        setState(draft => {
-          draft.wrongPasswordMessage = "WRONG PASSWORD"
-        })
-      }
+    if (wrongPassword) {
+      setState(draft => {
+        draft.renderWrongPasswordMessage = true
+      })
     }
+  }
+
+  useEffect(() => {
+    if (readyToEnter) {
+      navigate(`/room/${room.id}`)
+    }
+  }, [readyToEnter, navigate])
+
+  function renderWrongPasswordMessage() {
+    return (
+      <a
+        style={{
+          position: "relative",
+          width: "50px",
+          textAlign: "center",
+          fontVariant: "all-small-caps",
+          whiteSpace: "nowrap"
+        }}
+      >
+        <CSSTransition in={state.renderWrongPasswordMessage} timeout={330} classNames="liveValidateMessage" unmountOnExit>
+          <div className="alert alert-danger small liveValidateMessage" style={{ top: "-37px", left: "83px" }}>
+            wrong password
+          </div>
+        </CSSTransition>
+      </a>
+    )
   }
 
   function showPasswordInputActionOrJustEnter() {
@@ -98,7 +101,7 @@ function Room(props) {
       })
       return
     }
-    enterTheRoom()
+    initializeJoiningTheRoom()
     appDispatch({ type: "closeSearch" })
   }
 
@@ -107,6 +110,7 @@ function Room(props) {
       <div className="ml-auto d-flex">
         {renderWrongPasswordMessage()}
         <input
+          key={state.roomId}
           autoFocus
           type="password"
           className="room-password-input placeholder-color"
@@ -115,11 +119,12 @@ function Room(props) {
           onChange={e =>
             setState(draft => {
               draft.providedRoomPassword = e.target.value
-              draft.wrongPasswordMessage = ""
+              draft.renderWrongPasswordMessage = false
             })
           }
         />
         {renderPasswordSubmitButton()}
+        {renderPasswordCancelButton()}
       </div>
     )
   }
@@ -130,7 +135,7 @@ function Room(props) {
         className="btn-secondary ml-2 pr-2 pl-2"
         type="submit"
         onClick={() => {
-          enterTheRoom()
+          initializeJoiningTheRoom()
           appDispatch({ type: "closeSearch" })
         }}
       >
@@ -139,23 +144,19 @@ function Room(props) {
     )
   }
 
-  function renderWrongPasswordMessage() {
+  function renderPasswordCancelButton() {
     return (
-      <a
-        style={{
-          position: "relative",
-          width: "50px",
-          textAlign: "center",
-          fontVariant: "all-small-caps",
-          whiteSpace: "nowrap"
+      <button
+        className="btn-secondary ml-2 pr-2 pl-2"
+        type="reset"
+        onClick={() => {
+          setState(draft => {
+            draft.showPasswordInput = false
+          })
         }}
       >
-        <CSSTransition in={state.wrongPasswordMessage.length > 0} timeout={330} classNames="liveValidateMessage" unmountOnExit>
-          <div className="alert alert-danger small liveValidateMessage" style={{ top: "-37px", left: "83px" }}>
-            wrong password
-          </div>
-        </CSSTransition>
-      </a>
+        CANCEL
+      </button>
     )
   }
 
@@ -163,7 +164,7 @@ function Room(props) {
     <div className="single-room container mt-4">
       <div className="single-room-content container d-flex ml-3 p-2 align-items-center">
         <div id="room-name">
-          <Link onClick={() => enterTheRoom()}>{props.room.name}</Link>
+          <Link onClick={() => initializeJoiningTheRoom()}>{props.room.name}</Link>
         </div>
         <div className="ml-auto d-flex">{state.showPasswordInput && !state.isUserInRoom && renderPasswordInput()}</div>
         <div className="ml-auto d-flex">
